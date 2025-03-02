e07db5f0a04ac42480b1d73f2e10b655dc2c3a6c
Zakhar Guskov <guskov.zahar20@gmail.com>
2025-03-02 21:42:29 +0500

    add: token repository impl

--- back/tokenService/internal/domain/token/repository.go
+++ back/tokenService/internal/domain/token/repository.go
@@ -1,4 +1,4 @@
-package token
+package domain
 
 import "context"
 
@@ -7,6 +7,4 @@ type Repository interface {
 	FindAll(ctx context.Context) (token []Token, err error)
 	FindById(ctx context.Context, tokenId string) (Token, error)
 	FindBySymbol(ctx context.Context, symbol string) (Token, error)
-	Update(ctx context.Context, token Token) error
-	Delete(ctx context.Context, tokenId string) error
 }
---
+++ back/tokenService/internal/infrastructure/db/postgres.go
@@ -0,0 +1,118 @@
+package db
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/jackc/pgx/v5/pgconn"
+	"log/slog"
+	"strings"
+	"tokenService/internal/domain/token"
+	"tokenService/pkg/client/postgres"
+)
+
+type repository struct {
+	client postgres.Client
+	log    *slog.Logger
+}
+
+func (r *repository) Create(ctx context.Context, token *domain.Token) error {
+	q := `
+INSERT INTO token 
+	(name, symbol, decimals, contract_address, owner_wallet_address, date_of_create)
+VALUES
+    ($1, $2, $3, $4, $5, $6)
+RETURNING id;
+`
+	r.log.Debug(fmt.Sprintf("SQL Request: %s", formatQuery(q)))
+	if err := r.client.QueryRow(ctx, q, token.Name, token.Symbol, token.Decimals, token.ContractAddress, token.OwnerWalletAddress, token.DateOfCreate).Scan(&token.ID); err != nil {
+		var pgErr *pgconn.PgError
+		if errors.Is(err, pgErr) {
+			errors.As(err, &pgErr)
+			newErr := fmt.Errorf(fmt.Sprintf("SQL Error: %s, Detail: %s, Where: %s, Code: %s, SQLState: %s", pgErr.Error(), pgErr.Detail, pgErr.Where, pgErr.Code, pgErr.SQLState))
+			r.log.Error(newErr.Error())
+			return newErr
+		}
+	}
+
+	return nil
+}
+
+func (r *repository) FindAll(ctx context.Context) (token []domain.Token, err error) {
+	q := `
+SELECT 
+	id, name, symbol, decimals, contract_address, owner_wallet_address, date_of_create, date_of_update
+FROM token
+`
+
+	r.log.Debug(fmt.Sprintf("SQL Request: %s", formatQuery(q)))
+	rows, err := r.client.Query(ctx, q)
+	if err != nil {
+		return nil, err
+	}
+
+	tokens := make([]domain.Token, 0)
+	for rows.Next() {
+		var tkn domain.Token
+
+		err = rows.Scan(&tkn.ID, &tkn.Name, &tkn.Symbol, &tkn.Decimals, &tkn.ContractAddress, &tkn.OwnerWalletAddress, &tkn.DateOfCreate, &tkn.DateOfUpdate)
+		if err != nil {
+			return nil, err
+		}
+
+		tokens = append(tokens, tkn)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return tokens, nil
+}
+
+func (r *repository) FindById(ctx context.Context, tokenId string) (domain.Token, error) {
+	q := `
+SELECT 
+	id, name, symbol, decimals, contract_address, owner_wallet_address, date_of_create, date_of_update
+FROM token
+WHERE id = $1
+`
+
+	r.log.Debug(fmt.Sprintf("SQL Request: %s", formatQuery(q)))
+	var tkn domain.Token
+	err := r.client.QueryRow(ctx, q, tokenId).Scan(&tkn.ID, &tkn.Name, &tkn.Symbol, &tkn.Decimals, &tkn.ContractAddress, &tkn.OwnerWalletAddress, &tkn.DateOfCreate, &tkn.DateOfUpdate)
+	if err != nil {
+		return domain.Token{}, err
+	}
+
+	return tkn, nil
+}
+
+func (r *repository) FindBySymbol(ctx context.Context, symbol string) (domain.Token, error) {
+	q := `
+SELECT 
+	id, name, symbol, decimals, contract_address, owner_wallet_address, date_of_create, date_of_update
+FROM token
+WHERE symbol = $1
+`
+
+	r.log.Debug(fmt.Sprintf("SQL Request: %s", formatQuery(q)))
+	var tkn domain.Token
+	err := r.client.QueryRow(ctx, q, symbol).Scan(&tkn.ID, &tkn.Name, &tkn.Symbol, &tkn.Decimals, &tkn.ContractAddress, &tkn.OwnerWalletAddress, &tkn.DateOfCreate, &tkn.DateOfUpdate)
+	if err != nil {
+		return domain.Token{}, err
+	}
+
+	return tkn, nil
+}
+
+func NewRepository(client postgres.Client, log *slog.Logger) domain.Repository {
+	return &repository{
+		client: client,
+		log:    log,
+	}
+}
+
+func formatQuery(q string) string {
+	return strings.ReplaceAll(strings.ReplaceAll(q, "\t", ""), "\n", " ")
+}
